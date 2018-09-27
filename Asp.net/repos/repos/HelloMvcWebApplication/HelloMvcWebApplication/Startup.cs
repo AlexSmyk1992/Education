9526ed79e7e51a58081db276a2f4c4cbb061a467
AlexSmyk <alexoya2@mail.ru>
2018-09-27 18:18:55 +0300

    Add ASP.net
    Add ASP.net education

---
+++ Asp.net/repos/repos/HelloMvcWebApplication/HelloMvcWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloMvcWebApplication.Startup))]
+namespace HelloMvcWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
